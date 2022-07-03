ab46dcc878ae860c579fcee283dba417225b7772
Kyle Rohn <kylerohn0@gmail.com>
2022-07-02 22:03:28 -0600

    1st

---
+++ spotianalyze/spotianalyze.py
@@ -0,0 +1,156 @@
+import csv
+import os
+import json
+import spotipy
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+import psutil
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Spotianalyze:
+
+    # Class Vars
+    ALBUM = 'album'
+    ARTISTS = 'artists'
+    AVAILABLE_MARKETS = 'available_markets'
+    DISC_NUMBER = 'disc_number'
+    DURATION = 'duration'
+    DURATION_MS = 'duration_ms'
+    EXPLICIT = 'explicit'
+    EXTERNAL_IDS = 'external_ids'
+    EXTERNAL_URLS = 'external_urls'
+    HREF = 'href'
+    ID = 'id'
+    IS_LOCAL = 'is_local'
+    ITEM = 'item'
+    ITEMS = 'items'
+    NAME = 'name'
+    POPULARITY = 'popularity'
+    PREVIEW_URL = 'preview_url'
+    PROGRESS_MS = 'progress_ms'
+    SONG_INFO = 'song_info'
+    SONG_FEATURES = 'song_features'
+    TIME_ELAPSED = 'time_elapsed'
+    TIMES_LISTENED = 'times_listened'
+    TIMES_SKIPPED = 'times_skipped'
+    TOTAL = 'total'
+    TRACK = 'track'
+    TRACK_NUMBER = 'track_number'
+    TRACK_URI = 'track_uri'
+    TYPE = 'type'
+    URI = 'uri'
+
+    ##################################################################################################################################
+
+
+    DANCEABILITY = 'danceability'
+    ENERGY = 'energy'
+    KEY = 'key'
+    LOUDNESS = 'loudness'
+    MODE = 'mode'
+    SPEECHINESS = 'speechiness'
+    ACOUSTICNESS = 'acousticness'
+    INSTRUMENTALNESS = 'instrumentalness'
+    LIVENESS = 'liveness'
+    VALENCE = 'valence'
+    TEMPO = 'tempo'
+    TRACK_HREF = 'track_href'
+    ANALYSIS_URL = 'analysis_url'
+    TIME_SIGNATURE = 'time_signature'
+
+    KEYLIST = [DANCEABILITY, ENERGY, KEY, LOUDNESS, SPEECHINESS, ACOUSTICNESS, INSTRUMENTALNESS, LIVENESS, VALENCE, TEMPO, TIME_SIGNATURE]
+
+##################################################################################################################################
+# Initialize User 
+    def __init__(self, username: str, client_id: str, client_secret: str, redirect_uri: str):
+
+        self.USERNAME = username
+        self.CLIENT_ID = client_id
+        self.CLIENT_SECRET = client_secret
+        self.REDIERECT_URI = redirect_uri
+
+        os.environ['SPOTIPY_CLIENT_ID'] = client_id
+        os.environ['SPOTIPY_CLIENT_SECRET'] = client_secret
+        os.environ['SPOTIPY_REDIRECT_URI'] = redirect_uri
+
+        scope = 'user-read-private user-read-playback-state playlist-modify-public user-library-modify user-library-read'
+        try:
+            token = util.prompt_for_user_token(username, scope)
+        except(AttributeError, JSONDecodeError):
+            os.remove(f".cache-{username}")
+            token = util.prompt_for_user_token(username, scope)
+        spotify_object = spotipy.Spotify(auth=token)
+        user = spotify_object.current_user()
+        self.SPOTIFY_OBJECT = spotify_object
+
+    def __repr__(self):
+        return self.SPOTIFY_OBJECT
+
+    
+##################################################################################################################################
+# Get csv of relevant data (song name, song artist(s), song id, features, times listened, times skipped) using dict
+    def create_csv(self, filename: str):
+        # Initialize fn vars
+        page = 1
+        song_list = []
+        full_song_data = []
+
+        # Create list of all songs in library
+        while len(self.SPOTIFY_OBJECT.current_user_saved_tracks(limit=20, offset=(page-1))['items']) != 0:
+            temp_song_list = ((self.SPOTIFY_OBJECT.current_user_saved_tracks(limit=20, offset=(page-1))['items']))
+            song_list = song_list + temp_song_list
+            page = page + 20
+
+        # Create list of dicts for each song and associated data
+        it = 1
+        for track in song_list:
+            # Display Progress
+            print(f"{(it/len(song_list)) * 100}%", end=' ')
+            print('RAM memory % used:', psutil.virtual_memory()[2])
+
+            # Iteration Reqs
+            it = it + 1
+            track_uri = track[self.TRACK][self.ID]
+            track_features = self.SPOTIFY_OBJECT.audio_features(track_uri)
+
+            # Dict Creation
+            full_dict = {
+                self.NAME: track[self.TRACK][self.NAME],
+                self.ARTISTS: track[self.TRACK][self.ARTISTS],
+                self.ID: track_uri,
+                self.DURATION_MS: track[self.TRACK][self.DURATION_MS],
+                self.DANCEABILITY: track_features[0][self.DANCEABILITY],
+                self.ENERGY: track_features[0][self.ENERGY],
+                self.KEY: track_features[0][self.KEY],
+                self.LOUDNESS: track_features[0][self.LOUDNESS],
+                self.SPEECHINESS: track_features[0][self.SPEECHINESS],
+                self.INSTRUMENTALNESS: track_features[0][self.INSTRUMENTALNESS],
+                self.ACOUSTICNESS: track_features[0][self.ACOUSTICNESS],
+                self.LIVENESS: track_features[0][self.LIVENESS],
+                self.VALENCE: track_features[0][self.VALENCE],
+                self.TEMPO: track_features[0][self.TEMPO],
+                self.TIME_SIGNATURE: track_features[0][self.TIME_SIGNATURE],
+                self.TIMES_LISTENED: 0,
+                self.TIMES_SKIPPED: 0
+            }
+            print(full_dict)
+            full_song_data.append(full_dict)
+
+        # Create csv file with data
+        with open(filename, 'w') as outfile:
+            writer = csv.DictWriter(outfile, fieldnames=[self.NAME, self.ARTISTS, self.ID, self.DURATION_MS ] + self.KEYLIST + [self.TIMES_LISTENED, self.TIMES_SKIPPED])
+            writer.writeheader()
+            writer.writerows(full_song_data)
+            
+
+
+
+spotianalyze = Spotianalyze('kyler4646', '4fd6158dd6e34661a9189a2cb2122445', 'ee472e4cf73743009fec5d6fb827a8c1', 'https://google.com/')
+print(spotianalyze.create_csv('kyles_lib.csv'))
+
+    # Fns to get current playing info in dict format
+    # Fns to loop
+    # Create way to extract data easily
+    # Use that data to do stuff
+    
