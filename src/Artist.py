b5e0e2801fdb004077bc1fa9991e92019d1dab56
kylerohn <108435516+kylerohn@users.noreply.github.com>
2024-02-22 11:29:13 -0700

    Artist class

---
+++ src/Artist.py
@@ -0,0 +1,8 @@
+
+
+class Artist:
+    def __init__(self, name: str, genres: list, popularity: float, uri: str):
+        self._name = name
+        self._genres = genres
+        self._popularity = popularity
+        self._uri = uri
