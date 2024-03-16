7e72ecb9c81daac6352dd4a292fcf2b338df224b
kylerohn <108435516+kylerohn@users.noreply.github.com>
2024-03-16 16:07:38 -0600

    more cluster stuff

--- src/Cluster.py
+++ src/Cluster.py
@@ -1,9 +1,74 @@
+import numpy as np
+
 
 class Cluster:
+    """
+    Class to hold clusters of data for statistical analysis
+    """
+
+
     def __init__(self, *args):
+        """
+        Initializes Cluster object with list or collection of lists
+
+        Args: 
+            *args (List): Lists of data elements of the same length
+
+        Returns
+        """
         self.cluster = []
+        for arg in args:
+            self.cluster.append(arg)
+
     
-    def merge():
-        pass
+    def merge(self, merge_cluster):
+        """
+        Merges the current cluster with another cluster.
+
+        Args:
+            merge_cluster (Cluster): The cluster to merge with.
+
+        Returns:
+            None
+
+        Modifies:
+            Updates the cluster attribute of the current object by appending 
+            the cluster attribute of the merge_cluster object.
+
+        Note:
+            This method assumes that the merge_cluster parameter is an object 
+            of a class with a 'cluster' attribute representing a cluster of data.
+        """
+        self.cluster.append(merge_cluster.cluster)
     
-    
+
+    def avg_features(self):
+        """
+        Calculates the average feature values across all data points in the cluster.
+
+        Returns:
+            numpy.ndarray: An array containing the average feature values.
+
+        Note:
+            This method assumes that the cluster attribute of the instance contains
+            data points with consistent feature dimensions.
+
+        Example:
+            If the cluster attribute contains the following data points:
+            [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+
+            The returned array would be:
+            [4.0, 5.0, 6.0]
+        """
+        # turn current cluster into numpy array
+        np_cluster = np.array(self.cluster)
+
+        # sum columns
+        sums = np.sum(np_cluster, axis=0)
+
+        # divide each element by the length of the cluster
+        sums /= len(self.cluster)
+
+        return sums   
--- src/HierarchicalClustering.py
+++ src/HierarchicalClustering.py
@@ -1,28 +1,14 @@
 import numpy as np
 from pandas import DataFrame
+from Cluster import Cluster
 
 class HierarchicalCluster:
     def __init__(self, cluster_data: DataFrame, features: list):
 
-        self.feature_matrix = cluster_data[features].to_numpy()
-        self._cluster()
-
+        
+        self.feature_matrix: np.array = cluster_data[features].to_numpy()
+        self.clusters: list[Cluster] = []
+        for row in self.feature_matrix:
+            self.clusters.append(Cluster(row))
+        
     
-    def _cluster(self):
-        for idxs in range(0, self.feature_matrix.shape[0], 2):
-            min_distance = 1000
-            for idxc in range(idxs + 1, self.feature_matrix.shape[0]):
-                distance = self._euclidian_distance(self.feature_matrix[idxs], self.feature_matrix[idxc])
-                if min_distance > distance:
-                    min_distance = distance
-                    min_idx = idxc
-
-            self.feature_matrix[idxs+1], self.feature_matrix[min_idx] = self.feature_matrix[min_idx], self.feature_matrix[idxs+1]
-            pass
-    
-    def _euclidian_distance(self, arr, arr2):
-        sum = 0
-        for idx, val in enumerate(arr):
-            sum +=np.power(val-arr2[idx], 2)
-        return np.sqrt(sum)
-
