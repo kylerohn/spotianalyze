bd5ffc1188d3b3578a986717e95c10ca0178a46a
kylerohn <108435516+kylerohn@users.noreply.github.com>
2024-03-17 16:46:52 -0600

    sIzE IsSuE wItH aRrAys

--- src/HierarchicalClustering.py
+++ src/HierarchicalClustering.py
@@ -12,15 +12,33 @@ class HierarchicalCluster:
             self.clusters.append(Cluster(row))
     
 
-    def euclidian_cluster(self):
-        for cluster in (self.clusters):
-            
-
-    def _euclidian_distance(self, arr1: list, arr2: list):
+    def _euclidian_distance(self, arr1, arr2):
         sum: float = 0
-        for idx: int, val: float in arr1:
+        for idx, val in enumerate(arr1):
             sum += np.power(val - arr2[idx], 2)
         return np.sqrt(sum)
+
+
+    def euclidian_cluster(self):
+        new_clusters = []
+        for cluster in (self.clusters):
+            print(len(self.clusters))
+            min_distance = 1000
+            curr_idx = 0
+            for idx in range(1, len(self.clusters)):
+                # print(len(self.clusters[idx].cluster), idx)
+                curr_dist =  self._euclidian_distance(cluster.avg_features(), self.clusters[idx].avg_features())
+                if curr_dist < min_distance:
+                    min_distance = curr_dist
+                    curr_idx = idx
+            new_clusters.append(cluster.merge(self.clusters[curr_idx]))
+            del self.clusters[0]
+            del self.clusters[curr_idx]
+            print(len(self.clusters))
+
+
+
+
 
             
     
